5fa6d500ff697cf603705edd951c5475f7ddf8d8
TzeYiing <ty@tzeyiing.com>
2024-01-02 23:53:44 +0800

    chore: version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugCaisson.MixProject do
   def project do
     [
       app: :plug_caisson,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
